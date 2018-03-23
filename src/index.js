52798a9fc0371a4c39f6c5d6efa26b3ded6541a3
Alexey Popov <alexey.popov@umbrella-web.com>
2018-03-23 21:31:14 +0300

    observabels and subscriptions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
 const button = document.querySelector('button');
-Rx.Observable.fromEvent(button, 'click')
-  .throttleTime(1000)
-  .map((data) => {
-    return data.clientY;
-  })
-  .subscribe((coordinate) => console.log(coordinate));
+const observer = {
+  next: (value) => {
+    console.log(value);
+  },
+  error: (error) => {
+    console.log(error);
+  },
+  complete: () => {
+    console.log('Completed');
+  },
+};
+
+// Rx.Observable.fromEvent(button, 'click')
+//   .subscribe(observer);
+
+const subscription = Rx.Observable.create((obs) => {
+  // obs.next('A value');
+  // obs.error('Error');
+  // obs.complete();
+  // setTimeout(() => {
+  //   obs.next('A second value');
+  //   obs.complete();
+  // }, 2000)
+  button.onclick = function (event) {
+    obs.next(event);
+  }
+}).subscribe(observer);
+
+setTimeout(() => {
+  subscription.unsubscribe();
+}, 5000);
