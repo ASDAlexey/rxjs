79e31edb93272879cfa8df9d267720219df7d9be
Alexey Popov <alexey.popov@umbrella-web.com>
2018-03-24 00:28:14 +0300

    11 BehaviorSubject

--- src/index.js
+++ src/index.js
@@ -173,3 +173,23 @@ obs1.switchMap(
   }
 );
 */
+
+// 11 BehaviorSubject
+// const clickEmitted = new Rx.Subject();
+// const button = document.querySelector('button');
+// const div = document.querySelector('div');
+//
+// button.addEventListener('click', () => clickEmitted.next('Clicked!'));
+//
+// clickEmitted.subscribe(
+//   (value) => div.textContent = value
+// );
+//
+// clickEmitted.next('Not clicked');
+
+const clickEmitted = new Rx.BehaviorSubject('Not clicked');
+const button = document.querySelector('button');
+const div = document.querySelector('div');
+
+button.addEventListener('click', () => clickEmitted.next('Clicked!'));
+clickEmitted.subscribe(value => div.textContent = value);
