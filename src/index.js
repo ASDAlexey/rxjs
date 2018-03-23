34636528a383d3cfe43af14dafb2af31b3ed7e2c
Alexey Popov <alexey.popov@umbrella-web.com>
2018-03-24 00:09:20 +0300

    9 mergeObservable

--- src/index.js
+++ src/index.js
@@ -147,4 +147,14 @@ observable
   .distinctUntilChanged()
   .subscribe(observer);
 */
-// 9
+// 9 mergeObservable
+/*const input1 = document.querySelector('#input1');
+const input2 = document.querySelector('#input2');
+const span = document.querySelector('span');
+
+const observable1 = Rx.Observable.fromEvent(input1, 'input');
+const observable2 = Rx.Observable.fromEvent(input2, 'input');
+
+observable1.mergeMap(
+  event1 => (observable2.map(event2 => event1.target.value + ' ' + event2.target.value))
+).subscribe(combinedValue => (span.textContent = combinedValue));*/
